7ce3199eb0ffde1ff73753d297e6ce84dd21bbcd
tdyiufi <eric.gro.2000@gmail.com>
2024-09-06 12:54:42 +0000

    add import.env inn nest

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import { ConfigService } from "@nestjs/config"
+import * as cookieParser from "cookie-parser"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	const configService = app.get(ConfigService)
+
+	const frontendUrl = configService.get<string>("FRONTEND_URL") // Get FRONTEND_URL from .env
+
+	app.setGlobalPrefix("api")
+	app.use(cookieParser())
+	app.enableCors({
+		origin: [frontendUrl],
+		credentials: true,
+		exposedHeaders: "set-cookie"
+	})
 	await app.listen(7000)
 }
 bootstrap()
